21b51a38a42e1fae7a96979f9b0c907619415caf
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-18 17:21:35 -0700

    commit tree 30bb0814bb5eddb5a3dac8916fe607eb47a051c9

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("The DOM has loaded");
+//   const element = (document.getElementById("text").innerHTML =
+//     "This is really cool!");
+//   console.log(element);
+// });
+// console.log("testing");
+
+document.addEventListener("DOMContentLoaded", function () {
+  let newText = document.getElementById("text");
+  newText.textContent = "This is really cool!";
+});
